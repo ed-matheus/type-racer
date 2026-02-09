be6ebde014e635665f7a1f60cbc1ec037f2cb676
ed-matheus <edsonmatheus02@hotmail.com>
2026-02-09 20:48:07 -0300

    lógica de soma de erros finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -14,46 +14,25 @@ function App() {
 
   const [userInput, setUserInput] = useState("")
 
-  const fraseExemplo = "melancia"
-
-  const handleGetValue = (e:any) => {
-    e.preventDefault()
-    // console.log(userInput)
-
-    setUserInput("")
-    handleCheckValue()
-  }
-
-  const handleCheckValue = () => {
-    // console.log(userInput)
-
-    // Frase/palavra que o usuário digitou
-    const fraseDigitada = userInput.split("")
-    console.log(fraseDigitada[0])
-
-    // if (userInput !== fraseExemplo) {
-    //   console.log('ta errado')
-    // } else {
-    //   console.log('ta certo')
-    // }
-  }
+  const originalText = "paralelepipedo"
 
   return (
-    <>
-      <h1 className='text-blue-500'>Type Racer</h1>
-      <div>
-        <span>WPM: {stats.wpm}</span>
-        <span>Precisão: {stats.accuracy}</span>
-        <span>Erros: {stats.errorCount}</span>
+    <div className='p-5'>
+      <h1 className='font-bold'>Type Racer</h1>
+
+      <div className='flex flex-wrap items-center justify-center gap-3 font-bold mb-8'>
+        <span className='bg-white text-black px-2 py-1 rounded-sm'>WPM: {stats.wpm}</span>
+        <span className='bg-white text-black px-2 py-1 rounded-sm'>Precisão: {stats.accuracy}</span>
+        <span className='bg-white text-black px-2 py-1 rounded-sm'>Erros: {stats.errorCount}</span>
       </div>
 
       <div>
         {
-          fraseExemplo.split("").map((letraOriginal, i) => {
+          originalText.split("").map((originalLetter, i) => {
             // console.log(userInput)
             let cor;
             if (i < userInput.length) {
-              userInput[i] === letraOriginal ? cor = "text-green-500" : cor = "text-red-500"
+              userInput[i] === originalLetter ? cor = "text-green-500" : cor = "text-red-500"
             }
 
             return (
@@ -61,7 +40,7 @@ function App() {
                 key={i}
                 className={`text-gray-300 ${cor}`}
               >
-                {letraOriginal}
+                {originalLetter}
               </span>
             )
           })
@@ -70,16 +49,24 @@ function App() {
         <p>{userInput}</p>
       </div>
 
-      <form>
-        <input
-          className='border'
-          type="text"
-          value={userInput}
-          onChange={(e) => setUserInput(e.target.value)}
-        />
-        <button onClick={handleGetValue}>Enviar</button>
-      </form>
-    </>
+      <input
+        className='border'
+        type="text"
+        value={userInput}
+        onChange={(e) => {
+          setUserInput(e.target.value)
+
+          const currentText = e.target.value
+          const index = currentText.length - 1
+          
+          if (currentText.length > userInput.length) {
+            currentText[index] !== originalText[index] ? setStats({ ...stats, errorCount: stats.errorCount + 1 }) : ""
+          } else {
+            console.log('nenhum erro')
+          }
+        }}
+      />
+    </div>
   )
 }
 
